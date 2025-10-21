f77e6a568968e36eda33e4d472039a91b6492b8e
pphatdev <leatsophat02@gmail.com>
2025-10-21 19:11:48 +0700

    Update Person

---
+++ 1. Practice/Student.java
@@ -0,0 +1,21 @@
+public class Student extends Person
+{
+    public String studentID;
+
+    public void showStudent() {
+        System.out.println("Student ID: " + studentID);
+    }
+
+    public void main(String[] arg) {
+        Student student = new Student();
+        student.studentID = "TT02222";
+        student.name = "Leat Sophat";
+        student.gender = "Male";
+        student.nationalId = "101010101";
+        student.address = "Phnom Penh";
+        student.email = "info.sophat@gmail.com";
+        student.phone = "01010101010";
+        student.showStudent();
+        student.showPerson();
+    }
+}
